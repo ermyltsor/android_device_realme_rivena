6d874703049805fb1b28d9bb9d1907a0e8368a3a
Mashopy <eliasgheeraert@gmail.com>
2026-04-16 19:27:46 +0800

    rivena: Patch camera.qcom.so instead of com.qti.chi.override.so
    It seems that realme rely over camera.qcom.so for auxiliary camera availability.
    
    Test: Open CameraIDs app and all camera IDs are now showed.
          Open a custom Google Camera app and manually switch to each cameras.
    
    Change-Id: I6aa269a41bdc81405137bf6d1d81705470982488

--- extract-files.py
+++ extract-files.py
@@ -97,9 +97,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
-    'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
-        .add_needed('libcamera_metadata_shim.so')
-        .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
 }  # fmt: skip
