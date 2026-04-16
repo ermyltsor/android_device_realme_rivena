fc96beb883c3f3b22d923dd5a111f1b42d426a9d
Michael Bestas <mkbestas@lineageos.org>
2026-04-16 20:36:35 +0800

    rivena: Depend on android.hardware.graphics.common-V7
    Required for BP3A.
    
    Change-Id: Id071b20aa56d5c2e2717215fa6bb6f16f91198ec

--- extract-files.py
+++ extract-files.py
@@ -72,7 +72,7 @@ blob_fixups: blob_fixups_user_type = {
     ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libaps_frame_registration.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V7-ndk.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
